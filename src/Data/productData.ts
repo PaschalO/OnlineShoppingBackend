019eec93db224bee8a8ac0ccc5c6a96678c3f101
PaschalO <paschal.oduoza@gmail.com>
2023-07-16 03:43:25 -0400

    Added the data files

---
+++ src/Data/productData.ts
@@ -0,0 +1,28 @@
+import {Product} from "../utilities/types";
+export const productsData: Product[] =
+    [
+        {
+            name: "Apple Laptop",
+            price: 2499.99,
+            category: "Electronics",
+            description: "A brown laptop with 512GB, 16 RAM etc.",
+            image: "https://www.apple.com/ca/shop/buy-mac/macbook-pro/14-inch",
+            in_stock: false
+        },
+        {
+            name: "HP Laptop",
+            price: 1300.99,
+            category: "Electronics",
+            description: "A silver laptop with 512GB, 16 RAM etc.",
+            image: "https://www.apple.com/ca/shop/buy-mac/macbook-pro/14-inch",
+            in_stock: true
+        },
+        {
+            name: "Microsoft Laptop",
+            price: 1999.99,
+            category: "Electronics",
+            description: "A black laptop with 512GB, 16 RAM etc.",
+            image: "https://www.apple.com/ca/shop/buy-mac/macbook-pro/14-inch",
+            in_stock: true
+        },
+    ]
---
+++ src/Data/userData.ts
@@ -0,0 +1,27 @@
+import {User} from "../utilities/types";
+export const usersData: User[] =
+    [
+        {
+            firstname: "Rand",
+            lastname: "Jakoub",
+            email: "rjakoub3@altervista.org",
+            password: "dE4{onXy",
+            role: "Admin"
+        },
+        {
+            firstname: "Timi",
+            lastname: "Wavish",
+            email: "twavish4@aol.com",
+            password: "uR9#SwW,",
+            role: "Admin"
+        },
+        {
+            firstname: "Amaka",
+            lastname: "Dibo",
+            email: "amakadibo@aol.com",
+            password: "123456",
+            role: "user"
+        }
+    ]
+
+
